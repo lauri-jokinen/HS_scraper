8921df527920085f7d5a6b1a4ab1fc04b5373b44
Lauri Jokinen <65682962+lapamatoz@users.noreply.github.com>
2023-02-05 20:37:07 +0200

    Create HS_scraper.py

---
+++ HS_scraper.py
@@ -0,0 +1,20 @@
+import requests
+from datetime import datetime
+
+def paper_ID():
+    # Finds paper ID from the regular HS-website
+    # Here, we use today's paper
+    today_string = datetime.today().strftime('%d%m%Y')
+    paper_today_url = "https://www.hs.fi/paivanlehti/{}/".format(today_string) 
+    paper_today_html = str(requests.get(paper_today_url).content)
+    
+    # Fetch a 'nodeID' from the HTML code (this is magic)
+    return paper_today_html.split('nodeID":')[1].split(',')[0]     # returns, e.g., '3601'
+  
+def paper_contents():
+    # Uses hidden API to fetch the paper's contents
+    ID = paper_ID()
+    paper_contents_hidden_api_url = "https://www.hs.fi/api/editions/{}/toc".format(ID)
+    return requests.get(paper_contents_hidden_api_url).json()
+
+paper_contents()
