e441c3e1dd401f224fe524a269422bf3c736013a
Lauri Jokinen <65682962+lapamatoz@users.noreply.github.com>
2023-03-17 10:36:46 +0200

    fixed a bug in paper_ID

--- HS_scraper.py
+++ HS_scraper.py
@@ -9,7 +9,7 @@ def paper_ID():
     paper_today_html = str(requests.get(paper_today_url).content)
     
     # Fetch a 'nodeID' from the HTML code (this is magic)
-    return paper_today_html.split('nodeID":')[1].split(',')[0]     # returns, e.g., '3601'
+    return paper_today_html.split('nodeID":')[1].split(',')[0].split('}')[0]     # returns, e.g., '3601'
   
 def paper_contents():
     # Uses hidden API to fetch the paper's contents
